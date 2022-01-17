0d79495d2838b081c9719f689129be53b5fd24d9
anand  chauhan <anandchauhan8791@gmail.com>
2022-01-17 18:57:06 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,101 @@
+from logging import exception
+from operator import index
+from sqlite3 import Cursor
+from typing import Optional
+from fastapi import FastAPI,Response,status,HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+app = FastAPI()
+
+#conn= psycopg2(host='localhost',database='fastapidb',user='postgres',password='anand8791',cursor_factory=RealDictCursor)
+
+
+class Post(BaseModel):
+    title: str
+    content:str
+    published:bool = True
+while True:
+    try:
+        conn= psycopg2.connect(host='localhost',database='fastapidb',user='postgres',
+        password='anand8791',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('data base conncted...:)')
+        break  
+    except Exception as error:
+        print('conncetion is failed..:(')
+        print("Error:",error)
+        time.sleep(2)
+
+my_post = [{"title":"new post", "content":"no content yet","id":1},{"title":"fav food", "content":"checcken","id":2}]
+
+def find_post(id):
+    for p in my_post:
+        if p["id"] == id:
+            return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello fuccerrr"}
+
+@app.get("/post")
+def get_post():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
+
+@app.post("/post", status_code=status.HTTP_201_CREATED)
+def c_post(post:Post):
+    post_dict = post.dict() 
+    post_dict['id'] = randrange(0,100000)
+    my_post.append(post_dict)
+    return{"data":post_dict}
+    
+'''
+@app.get("/post/newadded")
+def get_new_post():
+    post= my_post[len(my_post) - 1]
+    return {"New Added post":post}
+'''
+@app.get("/post/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail= f"post with id: {id} not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"massege":f"post with {id} not found"}
+    return {"post detile":post}
+
+@app.delete("/post/{id}",status_code = status.HTTP_204_NO_CONTENT)
+def del_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id: {id} note avelabel")
+    my_post.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/post/{id}")
+def update_post(id:int, post: Post):
+    #print(post)
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id: {id} note avelabel")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_post[index] = post_dict
+
+    return {"data":post_dict} 
